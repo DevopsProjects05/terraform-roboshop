60044511daad8b4c149c45226d51fe46e3053a47
Nuthan <Nuthan.gatla@gmail.com>
2025-06-08 00:54:49 +0530

    Initial Commit

--- 01-vpc/variables.tf
+++ 01-vpc/variables.tf
@@ -26,14 +26,21 @@ variable "environment" {
 }
 
 variable "public_subnets_cidr" {
+  description = "The CIDR blocks for the public subnets"
+  type        = list(string)
   default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnets_cidr" {
+  description = "The CIDR blocks for the private subnets"
+  type        = list(string)
   default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
+
 variable "database_subnets_cidr" {
+  description = "The CIDR blocks for the database subnets"
+  type        = list(string)
   default = ["10.0.21.0/24", "10.0.22.0/24"]
 }
 
